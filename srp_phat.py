1a934d6fdccae7d47fd0884d6a65d59db3bd34cd
zakarRoman <1208112917@qq.com>
2024-03-24 10:55:41 +0800

    拿到了圆形网格上各个角度的srp值

--- srp_phat.py
+++ srp_phat.py
@@ -14,6 +14,76 @@ def read_audio(filename):
     return _y, _fs
 
 
+def calculate_frequencies(yWin, fs):  # yWin代表信号在一个窗口时间段内的采样点, fs是信号的采样频率，表示每秒的采样次数
+    freq = np.fft.fftfreq(len(yWin), 1.0 / fs)  # 窗口内数据的频率
+    freq = freq[1:]
+    return freq
+
+
+def calculate_energy(yWin, fs):
+    ampl = np.abs(np.fft.fft(yWin))  # 快速傅立叶变换，abs用于获取结果的幅度，用来表示信号的能量
+    ampl = ampl[1:]  # 去掉幅度数组中的直流分量
+    energy = ampl ** 2  # 幅度数组平方，获得能量
+    return energy
+
+
+# 声音检测函数，用于处理语音信号
+def vad(y, fs, thrs):
+    # thrs:阈值
+    # 设置帧长度以及重叠
+    frame_length = 0.02
+    frame_overlap = 0.01
+    energy_thresh = thrs
+
+    frame = int(fs * frame_length)
+    frame_overlap = int(fs * frame_overlap)
+
+    detected_windows = np.array([])
+
+    si = 0  # 开始处理的索引
+    # 定义处理的语音频率范围（人声频率范围内）
+    s_band = 300
+    e_band = 3400
+
+    # 遍历数据，以帧为单位进行处理
+    while si < (len(y) - frame):  # 以帧为单位对信号进行处理
+        ei = si + frame  # 窗口结束索引
+
+        if ei >= len(y): ei = len(y) - 1  # 如果超出了数据末尾，就将结束帧设为数据末尾
+        yWin = y[si:ei]  # 取走数据帧
+
+        freqs = calculate_frequencies(yWin, fs)  # 计算这些帧的频率
+        energy = calculate_energy(yWin, fs)  # 计算这些帧的能量
+
+        # 使用能量 - 频率字典，用于存储每个频率成分的能量
+        energy_freq = {}
+        for (i, freq) in enumerate(freqs):
+            if abs(freq) not in energy_freq:
+                energy_freq[abs(freq)] = energy[i] * 2
+
+        # 计算最大能量
+        sum_max_energy = sum(energy_freq.values())
+
+        # 计算语音带的总能量
+        sum_energy = 0
+        for f in energy_freq.keys():
+            if s_band < f < e_band:  # 频率在人声范围内
+                sum_energy += energy_freq[f]
+
+        # 计算比率
+        ratio = sum_energy / sum_max_energy
+        # 与阈值结合进行判断
+        ratio = ratio > energy_thresh
+        # 输出每帧是否检测到语音的结果
+        detected_windows = np.append(detected_windows, [si, ratio])
+        si += frame_overlap  # 移动到下一帧
+
+    # 重塑输出数据
+    detected_windows = detected_windows.reshape(len(detected_windows) // 2, 2)
+    return detected_windows
+
+
+# srp-phat函数
 def srp_phat(s, L, fs, nfft=None, mode='far'):
     if nfft is None:
         nfft = 1024
@@ -28,12 +98,15 @@ def srp_phat(s, L, fs, nfft=None, mode='far'):
 
     # 进行SRP
     doa.locate_sources(s_FFT)
+    print(f'方位角的上srp的值为:{doa.grid.values}')
+    matrix = np.array(doa.grid.values)
+    print(matrix.shape)
 
-    doa.polar_plt_dirac()
-    plt.title('SRP_PHAT')
+    # doa.polar_plt_dirac()
+    # plt.title('SRP_PHAT')
     # 使用自带的方法画图
-    print('Speakers at: ', np.sort(doa.azimuth_recon) / np.pi * 180, 'degrees')
-    plt.show()
+    # print('Speakers at: ', np.sort(doa.azimuth_recon) / np.pi * 180, 'degrees')
+    # plt.show()
 
 
 if __name__ == '__main__':
